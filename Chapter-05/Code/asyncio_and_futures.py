d76b021f816c924be574ebc4f215101b19ce9251
SyedAliasad20 <syedaliasad05@gmail.com>
2026-06-13 22:25:28 +0500

    Added Chapter 05

---
+++ Chapter-05/Code/asyncio_and_futures.py
@@ -0,0 +1,40 @@
+import asyncio
+import sys
+
+async def first_coroutine(num):
+    count = 0
+    for i in range(1, num + 1):
+        count += 1
+
+    await asyncio.sleep(4)
+    return f"First coroutine (sum of N ints) result = {count}"
+
+
+async def second_coroutine(num):
+    count = 1
+    for i in range(2, num + 1):
+        count *= i
+
+    await asyncio.sleep(4)
+    return f"Second coroutine (factorial) result = {count}"
+
+
+def got_result(task):
+    print(task.result())
+
+
+async def main():
+    num1 = int(sys.argv[1])
+    num2 = int(sys.argv[2])
+
+    task1 = asyncio.create_task(first_coroutine(num1))
+    task2 = asyncio.create_task(second_coroutine(num2))
+
+    task1.add_done_callback(got_result)
+    task2.add_done_callback(got_result)
+
+    await asyncio.gather(task1, task2)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
